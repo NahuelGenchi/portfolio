ff71e1158b659a41dc821df2f36340f6bd250a58
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-18 22:30:11 -0300

    navbar: responsive design
    adding responsiveness to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,37 @@
 import "./Navbar.scss";
 
 const Navbar = function() {
-
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="#home" alt="nav icon">
+            <ion-icon name="home-outline"></ion-icon>
+          </a>
+        </li>
+        <li>
+          <a href="#home" alt="nav icon">
+            <ion-icon name="person-circle-outline"></ion-icon>
+          </a>
+        </li>
+        <li>
+          <a href="#home" alt="nav icon">
+            <ion-icon name="briefcase-outline"></ion-icon>
+          </a>
+        </li>
+        <li>
+          <a href="#home" alt="nav icon">
+            <ion-icon name="code-outline"></ion-icon>
+          </a>
+        </li>
+        <li>
+          <a href="#home" alt="nav icon">
+            <ion-icon name="mail-outline"></ion-icon>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
