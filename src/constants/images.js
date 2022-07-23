efb1a4a3f82a78cf58e40476e15bb4fbe555abeb
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-23 14:41:57 -0300

    improving project-container-img

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,15 @@
 import notesimg from "../assets/notes.png";
 import devinfo from "../assets/devinfo.png"
+import todolist from "../assets/todolist.jpg";
+import marsfacts from "../assets/marsfacts.png"
+import unitconversion from "../assets/unitconversion.png";
 
 const images = {
   notesimg,
-  devinfo
+  devinfo,
+  todolist,
+  marsfacts,
+  unitconversion
 };
 
 export default images;
--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -12,7 +12,10 @@ const Projects = function () {
         <h2 className="title">Projects</h2>
         <div className="projects-container">
           <ProjectContainer imglink={images.notesimg} alt="Notes App picture" host="https://nahuelgenchi.github.io/notes/" github="https://github.com/NahuelGenchi/notes.git" title="Notes App" desc="In this note app, users can easily add, edit, or delete their notes. The notes that has been added to this app will be stored in the browser's local storage so, they won't remove on page refresh or tab close."/>
+          <ProjectContainer imglink={images.todolist} alt="To-Do List picture" host="https://nahuelgenchi.github.io/tdlistb/" github="https://github.com/NahuelGenchi/tdlistb.git" title="To-Do List" desc="In this To-Do List you can add, delete and mark tasks as done. The tasks are stored in the local storage of the browser so the tasks won't be lost after refreshing the page." />
+          <ProjectContainer imglink={images.unitconversion} alt="Unit Conversion picture" host="https://nahuelgenchi.github.io/unitconverter/" github="https://github.com/NahuelGenchi/unitconverter.git" title="Unit Conversion" desc="In this app you can convert meters, feet, liters, gallons, kilos and pounds into their respective opposite metric/imperial unit." />
           <ProjectContainer imglink={images.devinfo} alt="Dev Info picture" host="https://nahuelgenchi.github.io/devinfo/" github="https://github.com/NahuelGenchi/devinfo.git" title="DevInfo" desc="DevInfo is an app that I created for development shortcuts. Here I can find resources from Git, SASS and Google Fonts."/>
+          <ProjectContainer imglink={images.marsfacts} alt="Mars Facts picture" host="https://nahuelgenchi.github.io/mars/" github="https://github.com/NahuelGenchi/mars.git" title="Mars Facts" desc="This was my first project since I started to learn front-end, it is an HTML-only website where you can find facts about the planet Mars. Never forget where you come from."/>
         </div>
       </div>
     </section>
