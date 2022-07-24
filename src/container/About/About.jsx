b55e356552589209ef8c98eafc42eac1227a7208
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-24 19:11:58 -0300

    #3 #4 done

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -7,7 +7,7 @@ const About = function () {
     <section id="about" className="section">
       <div className="section__container">
         <h2 className="title">About me</h2>
-        <p>Hi! I'm Nahuel Genchi and I'm from Buenos Aires, Argentina. I'm a Front-End Developer who is looking for his first job as a developer. I learned Front-End technologies on Coderhouse where I'm currently studying React, besides that I'm constantly learning new things on platforms like freeCodeCamp and MDN Web Docs.</p>
+        <p>My name is Nahuel Genchi and I'm from Buenos Aires, Argentina. I'm a passionate Front-End learner who is looking for his first job as a developer. I learned Front-End technologies on Coderhouse, where I'm currently studying React. Besides that, I'm constantly learning new things on platforms like freeCodeCamp and MDN Web Docs.</p>
         <p>My next career goal is to start studying Computer Engineering at <abbr title="Universidad Nacional de Tres de Febrero">UNTREF</abbr>.</p>
         <p>I'm currently working as an IT Support Technician at Nautec, which is an IT Services company.</p>
       </div>
