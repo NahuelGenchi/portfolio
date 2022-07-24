794bb92db63a1a2bef8cc3128cbede616d2d3b7a
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-24 11:41:20 -0300

    updating favicon & twimg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Header, About, Projects, Skills, Contact } from "./container";
 function App() {
   return (
     <div className="app">
-      <Favicon url="https://cdn-icons-png.flaticon.com/512/428/428001.png?v=ANYTHING" />
+      <Favicon url="https://github.com/NahuelGenchi/portfolio/blob/main/src/assets/favicon.png?v=ANYTHING" />
       <Topbar />
       <Header />
       <About />
