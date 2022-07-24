1408da18dc0227c3a87a0c4c113dd9dfe14b1a16
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-24 17:18:03 -0300

    uploading favicon files

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { Header, About, Projects, Skills, Contact } from "./container";
 function App() {
   return (
     <div className="app">
-      <Favicon url="https://raw.githubusercontent.com/NahuelGenchi/portfolio/main/src/assets/favicon.png" />
       <Topbar />
       <Header />
       <About />
