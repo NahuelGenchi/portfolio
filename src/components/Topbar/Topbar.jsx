50d98313b3314e1963ed1a9db7f7dff0abce145d
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-23 22:07:00 -0300

    responsive fixes

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -7,11 +7,11 @@ const Topbar = function () {
     <div className="topbar">
       <div className="topbar-container">
         <div className="topbar-name">
-          <p><ion-icon name="code-slash-outline"></ion-icon>Nahuel</p>
+          <p>Nahuel</p>
         </div>
         <Navbar />
         <div className="topbar-display">
-          <ion-icon name="sunny-outline"></ion-icon>
+          <ion-icon name="logo-react"></ion-icon>
         </div>
       </div>
     </div>
