6d69688f62b4878a7ad21b39ea0caadf7814572a
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-24 11:36:52 -0300

    uploading favicon and twimg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Favicon from "react-favicon";
+
 import { Topbar } from "./components";
 import { Header, About, Projects, Skills, Contact } from "./container";
 
 function App() {
   return (
     <div className="app">
+      <Favicon url="https://cdn-icons-png.flaticon.com/512/428/428001.png?v=ANYTHING" />
       <Topbar />
       <Header />
       <About />
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,21 +1,9 @@
-import notesimg from "../assets/notes.png";
-import devinfo from "../assets/devinfo.png"
-import todolist from "../assets/todolist.jpg";
-import marsfacts from "../assets/marsfacts.png"
-import unitconversion from "../assets/unitconversion.png";
 import git from "../assets/git.png";
 import linux from "../assets/linux.png";
-import windows from "../assets/windows.png";
 
 const images = {
-  notesimg,
-  devinfo,
-  todolist,
-  marsfacts,
-  unitconversion,
   git,
   linux,
-  windows
 };
 
 export default images;
