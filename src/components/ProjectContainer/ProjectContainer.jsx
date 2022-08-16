e220c9557d36ebfdf5f1bff56148d57b9514625f
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-08-16 08:40:25 -0300

    creating map of projectsData

--- src/components/ProjectContainer/ProjectContainer.jsx
+++ src/components/ProjectContainer/ProjectContainer.jsx
@@ -2,35 +2,38 @@ import projectsData from "../../mock/data";
 
 import "./ProjectContainer.scss";
 
-const ProjectContainer = function (props) {
-  const iconcolor = props.iconcolor;
+const ProjectContainer = () => {
   return (
     <>
-      <div className="project-container">
-        <div className="pc-picture">
-          <ion-icon name={props.icon} style={{ color: iconcolor }}></ion-icon>
-        </div>
-        <div className="pc-info">
-          <div className="pc-info-text">
-            <h3>{props.title}</h3>
-            <p>{props.desc}</p>
-          </div>
-          <div className="pc-info-assets">
-            <div className="pc-technologies-container">
+      {projectsData.map((project, index) => {
+        return (
+          <div className="project-container" key={index}>
+            <div className="pc-picture">
+              <ion-icon name={project.iconName} style={{ color: project.iconColor }}></ion-icon>
             </div>
-            <div className="pc-button-container">
-              <a href={props.host} target="_blank" rel="noreferrer">
-                <ion-icon name="link-outline"></ion-icon>
-                <span>Visit</span>
-              </a>
-              <a href={props.github} target="_blank" rel="noreferrer">
-                <ion-icon name="logo-github"></ion-icon>
-                <span>GitHub</span>
-              </a>
+            <div className="pc-info">
+              <div className="pc-info-text">
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+              </div>
+              <div className="pc-info-assets">
+                <div className="pc-technologies-container">
+                </div>
+                <div className="pc-button-container">
+                  <a href={project.hostLink} target="_blank" rel="noreferrer">
+                    <ion-icon name="link-outline"></ion-icon>
+                    <span>Visit</span>
+                  </a>
+                  <a href={project.githubLink} target="_blank" rel="noreferrer">
+                    <ion-icon name="logo-github"></ion-icon>
+                    <span>GitHub</span>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </>
   );
 };
--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -10,11 +10,7 @@ const Projects = function () {
       <div className="section__container">
         <h2 className="title">Projects</h2>
         <div className="projects-container">
-          <ProjectContainer icon="document-text" iconcolor="#FFE278" host="https://nahuelgenchi.github.io/notes/" github="https://github.com/NahuelGenchi/notes.git" title="Notes App" desc="In this note app, users can easily add, edit or delete their notes. The notes added to this app will be stored in the browser's local storage, so they won't be removed on page refresh or tab close."/>
-          <ProjectContainer icon="checkbox" iconcolor="#89E289" host="https://nahuelgenchi.github.io/tdlistb/" github="https://github.com/NahuelGenchi/tdlistb.git" title="To-Do List" desc="In this To-Do List you can add, delete and mark tasks as done. The tasks are stored in the browser's local storage in order to not be lost after refreshing the page" />
-          <ProjectContainer icon="swap-horizontal" iconcolor="white" host="https://nahuelgenchi.github.io/unitconverter/" github="https://github.com/NahuelGenchi/unitconverter.git" title="Unit Conversion" desc="In this app you can convert meters, feet, liters, gallons, kilos and pounds into their respective opposite metric/imperial unit." />
-          <ProjectContainer icon="terminal" iconcolor="#3597CB" host="https://nahuelgenchi.github.io/devinfo/" github="https://github.com/NahuelGenchi/devinfo.git" title="DevInfo" desc="DevInfo is an app that I created for development shortcuts. Here I can find resources from Git, SASS and Google Fonts."/>
-          <ProjectContainer icon="telescope" iconcolor="gray" host="https://nahuelgenchi.github.io/mars/" github="https://github.com/NahuelGenchi/mars.git" title="Mars Facts" desc="This was my first project since I started learning front-end. It is an HTML-only website where you can find facts about the planet Mars. Never forget where you come from."/>
+          <ProjectContainer/>
         </div>
       </div>
     </section>
