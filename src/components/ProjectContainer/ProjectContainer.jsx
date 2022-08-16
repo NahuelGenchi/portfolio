9ee5120f8a6fe655d6803d7c1cfa37e54e893fdd
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-08-16 08:34:12 -0300

    creating project data

--- src/components/ProjectContainer/ProjectContainer.jsx
+++ src/components/ProjectContainer/ProjectContainer.jsx
@@ -1,33 +1,37 @@
+import projectsData from "../../mock/data";
+
 import "./ProjectContainer.scss";
 
 const ProjectContainer = function (props) {
   const iconcolor = props.iconcolor;
   return (
-    <div className="project-container">
-      <div className="pc-picture">
-        <ion-icon name={props.icon} style={{color: iconcolor }}></ion-icon>
-      </div>
-      <div className="pc-info">
-        <div className="pc-info-text">
-          <h3>{props.title}</h3>
-          <p>{props.desc}</p>
+    <>
+      <div className="project-container">
+        <div className="pc-picture">
+          <ion-icon name={props.icon} style={{ color: iconcolor }}></ion-icon>
         </div>
-        <div className="pc-info-assets">
-          <div className="pc-technologies-container">
+        <div className="pc-info">
+          <div className="pc-info-text">
+            <h3>{props.title}</h3>
+            <p>{props.desc}</p>
           </div>
-          <div className="pc-button-container">
-            <a href={props.host} target="_blank" rel="noreferrer">
-              <ion-icon name="link-outline"></ion-icon>
-              <span>Visit</span>
-            </a>
-            <a href={props.github} target="_blank" rel="noreferrer">
-              <ion-icon name="logo-github"></ion-icon>
-              <span>GitHub</span>
-            </a>
+          <div className="pc-info-assets">
+            <div className="pc-technologies-container">
+            </div>
+            <div className="pc-button-container">
+              <a href={props.host} target="_blank" rel="noreferrer">
+                <ion-icon name="link-outline"></ion-icon>
+                <span>Visit</span>
+              </a>
+              <a href={props.github} target="_blank" rel="noreferrer">
+                <ion-icon name="logo-github"></ion-icon>
+                <span>GitHub</span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/mock/data.js
@@ -0,0 +1,44 @@
+const projectsData = [
+  {
+    iconName: "document-text",
+    iconColor: "#FFE278",
+    hostLink: "https://nahuelgenchi.github.io/notes",
+    githubLink: "https://github.com/NahuelGenchi/notes.git",
+    title: "Notes App",
+    description: "In this note app, users can easily add, edit or delete their notes. The notes added to this app will be stored in the browser's local storage, so they won't be removed on page refresh or tab close."
+  },
+  {
+    iconName: "checkbox",
+    iconColor: "#89E289",
+    hostLink: "https://nahuelgenchi.github.io/tdlistb",
+    githubLink: "https://github.com/NahuelGenchi/tdlistb.git",
+    title: "To-Do List",
+    description: "In this To-Do List you can add, delete and mark tasks as done. The tasks are stored in the browser's local storage in order to not be lost after refreshing the page"
+  },
+  {
+    iconName: "swap-horizontal",
+    iconColor: "#fff",
+    hostLink: "https://nahuelgenchi.github.io/unitconverter",
+    githubLink: "https://github.com/NahuelGenchi/unitconverter.git",
+    title: "Unit Conversion",
+    description: "In this app you can convert meters, feet, liters, gallons, kilos and pounds into their respective opposite metric/imperial unit."
+  },
+  {
+    iconName: "terminal",
+    iconColor: "#3597CB",
+    hostLink: "https://nahuelgenchi.github.io/devinfo",
+    githubLink: "https://github.com/NahuelGenchi/devinfo.git",
+    title: "DevInfo",
+    description: "DevInfo is an app that I created for development shortcuts. Here I can find resources from Git, SASS and Google Fonts."
+  },
+  {
+    iconName: "telescope",
+    iconColor: "#888",
+    hostLink: "https://nahuelgenchi.github.io/mars",
+    githubLink: "https://github.com/NahuelGenchi/mars.git",
+    title: "Mars Facts",
+    description: "This was my first project since I started learning front-end. It is an HTML-only website where you can find facts about the planet Mars. Never forget where you come from."
+  }
+];
+
+export default projectsData;
